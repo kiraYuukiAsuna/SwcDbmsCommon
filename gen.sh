20013cabc9c65da0f2fd1d6feff864145d70a079
kiraYuukiAsuna <kiraYuukiAsuna@outlook.com>
2024-03-18 17:31:43 +0800

    Update script.

--- gen.sh
+++ gen.sh
@@ -14,8 +14,8 @@ mkdir "apiref"
 mkdir "apiref/openapiv2"
 "Bin/protobuf/Linux/protoc" -I=Bin/protobuf/include -I=proto proto/Service/* -I=Bin/protobuf/include/protoc-gen-openapiv2/options --plugin=protoc-gen-openapiv2=Bin/protobuf/Linux/protoc-gen-openapiv2 --openapiv2_out apiref/openapiv2 --openapiv2_opt generate_unbound_methods=true
 
-mv "Generated/go/DBMS/SwcDbmsCommon/Generated/go/proto/message" "Generated/go/proto"
-mv "Generated/go/Service/Service.pb.gw.go" "Generated/go/proto/service"
+mv "Generated/go/DBMS/SwcDbmsCommon/Generated/go/proto" "Generated/go/proto"
+mv "Generated/go/Service/Service.pb.gw.go" "Generated/go/proto/service/Service.pb.gw.go"
 rm -rf "Generated/go/DBMS"
 rm -rf "Generated/go/Service"
 
