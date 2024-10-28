4b1a6675e5b2a557887d3f1b3d05db916c7e27c2
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2024-10-28 17:08:16 +0800

    Update build script.

---
+++ GenCpp.ps1
@@ -0,0 +1,30 @@
+param (
+    [string]$ProtobufPath = "Bin/protobuf/Windows"
+)
+
+function Remove-Directory {
+    param (
+        [string]$Path
+    )
+    
+    if (Test-Path $Path) {
+        Remove-Item -Recurse -Force -Path $Path
+    }
+}
+
+Remove-Directory "Generated/cpp"
+New-Item -Path "Generated/cpp" -ItemType Directory -Force
+
+& "$ProtobufPath/protoc.exe" `
+    -I=Bin/protobuf/include `
+    -I=proto proto/Message/* `
+    --plugin=protoc-gen-grpc=$ProtobufPath/grpc_cpp_plugin.exe `
+    --cpp_out=Generated/cpp `
+    --grpc_out=Generated/cpp
+
+& "$ProtobufPath/protoc.exe" `
+    -I=Bin/protobuf/include `
+    -I=proto proto/Service/* `
+    --plugin=protoc-gen-grpc=$ProtobufPath/grpc_cpp_plugin.exe `
+    --cpp_out=Generated/cpp `
+    --grpc_out=Generated/cpp
---
+++ GenGo.ps1
@@ -0,0 +1,57 @@
+param (
+    [string]$ProtobufPath = "Bin/protobuf/Windows"
+)
+
+function Remove-Directory {
+    param (
+        [string]$Path
+    )
+    
+    if (Test-Path $Path) {
+        Remove-Item -Recurse -Force -Path $Path
+    }
+}
+
+Remove-Directory "Generated/go"
+New-Item -Path "Generated/go" -ItemType Directory -Force
+
+& "$ProtobufPath/protoc.exe" `
+    -I=Bin/protobuf/include `
+    -I=proto proto/Message/* `
+    --plugin=protoc-gen-go=$ProtobufPath/protoc-gen-go.exe `
+    --plugin=protoc-gen-go-grpc=$ProtobufPath/protoc-gen-go-grpc.exe `
+    --go_out=Generated/go `
+    --go-grpc_out=Generated/go
+
+& "$ProtobufPath/protoc.exe" `
+    -I=Bin/protobuf/include `
+    -I=proto proto/Service/* `
+    --plugin=protoc-gen-go=$ProtobufPath/protoc-gen-go.exe `
+    --plugin=protoc-gen-go-grpc=$ProtobufPath/protoc-gen-go-grpc.exe `
+    --go_out=Generated/go `
+    --go-grpc_out=Generated/go
+
+& "$ProtobufPath/protoc.exe" `
+    -I=Bin/protobuf/include `
+    -I=proto proto/Service/* `
+    --plugin=protoc-gen-grpc-gateway=$ProtobufPath/protoc-gen-grpc-gateway.exe `
+    --grpc-gateway_out=Generated/go `
+    --grpc-gateway_opt=paths=source_relative `
+    --grpc-gateway_opt=generate_unbound_methods=true
+
+Remove-Directory "apiref"
+New-Item -Path "apiref/openapiv2" -ItemType Directory -Force
+
+& "$ProtobufPath/protoc.exe" `
+    -I=Bin/protobuf/include `
+    -I=proto proto/Service/* `
+    -I=Bin/protobuf/include/protoc-gen-openapiv2/options `
+    --plugin=protoc-gen-openapiv2=$ProtobufPath/protoc-gen-openapiv2.exe `
+    --openapiv2_out=apiref/openapiv2 `
+    --openapiv2_opt=generate_unbound_methods=true
+
+Move-Item -Path "Generated/go/DBMS/SwcDbmsCommon/Generated/go/proto" -Destination "Generated/go/proto"
+Move-Item -Path "Generated/go/Service/Service.pb.gw.go" -Destination "Generated/go/proto/service"
+
+Remove-Directory "Generated/go/DBMS"
+Remove-Directory "Generated/go/Service"
