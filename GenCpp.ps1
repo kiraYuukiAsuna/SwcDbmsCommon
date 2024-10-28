290a35a32b62faf5a4e0a31b86c4a47017cc571a
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2024-10-28 17:24:54 +0800

    Update.

--- GenCpp.ps1
+++ GenCpp.ps1
@@ -1,5 +1,6 @@
 param (
-    [string]$ProtobufPath = "Bin/protobuf/Windows"
+    [string]$ProtobufPath = "Bin/protobuf/Windows",
+    [string]$gRPCPath = "Bin/protobuf/Windows"
 )
 
 function Remove-Directory {
@@ -18,13 +19,13 @@ New-Item -Path "Generated/cpp" -ItemType Directory -Force
 & "$ProtobufPath/protoc.exe" `
     -I=Bin/protobuf/include `
     -I=proto proto/Message/* `
-    --plugin=protoc-gen-grpc=$ProtobufPath/grpc_cpp_plugin.exe `
+    --plugin=protoc-gen-grpc=$gRPCPath/grpc_cpp_plugin.exe `
     --cpp_out=Generated/cpp `
     --grpc_out=Generated/cpp
 
 & "$ProtobufPath/protoc.exe" `
     -I=Bin/protobuf/include `
     -I=proto proto/Service/* `
-    --plugin=protoc-gen-grpc=$ProtobufPath/grpc_cpp_plugin.exe `
+    --plugin=protoc-gen-grpc=$gRPCPath/grpc_cpp_plugin.exe `
     --cpp_out=Generated/cpp `
     --grpc_out=Generated/cpp
--- GenGo.ps1
+++ GenGo.ps1
@@ -1,5 +1,6 @@
 param (
-    [string]$ProtobufPath = "Bin/protobuf/Windows"
+    [string]$ProtobufPath = "Bin/protobuf/Windows",
+    [string]$gRPCPath = "Bin/protobuf/Windows"
 )
 
 function Remove-Directory {
@@ -19,7 +20,7 @@ New-Item -Path "Generated/go" -ItemType Directory -Force
     -I=Bin/protobuf/include `
     -I=proto proto/Message/* `
     --plugin=protoc-gen-go=$ProtobufPath/protoc-gen-go.exe `
-    --plugin=protoc-gen-go-grpc=$ProtobufPath/protoc-gen-go-grpc.exe `
+    --plugin=protoc-gen-go-grpc=$gRPCPath/protoc-gen-go-grpc.exe `
     --go_out=Generated/go `
     --go-grpc_out=Generated/go
 
@@ -27,14 +28,14 @@ New-Item -Path "Generated/go" -ItemType Directory -Force
     -I=Bin/protobuf/include `
     -I=proto proto/Service/* `
     --plugin=protoc-gen-go=$ProtobufPath/protoc-gen-go.exe `
-    --plugin=protoc-gen-go-grpc=$ProtobufPath/protoc-gen-go-grpc.exe `
+    --plugin=protoc-gen-go-grpc=$gRPCPath/protoc-gen-go-grpc.exe `
     --go_out=Generated/go `
     --go-grpc_out=Generated/go
 
 & "$ProtobufPath/protoc.exe" `
     -I=Bin/protobuf/include `
     -I=proto proto/Service/* `
-    --plugin=protoc-gen-grpc-gateway=$ProtobufPath/protoc-gen-grpc-gateway.exe `
+    --plugin=protoc-gen-grpc-gateway=$gRPCPath/protoc-gen-grpc-gateway.exe `
     --grpc-gateway_out=Generated/go `
     --grpc-gateway_opt=paths=source_relative `
     --grpc-gateway_opt=generate_unbound_methods=true
@@ -46,7 +47,7 @@ New-Item -Path "apiref/openapiv2" -ItemType Directory -Force
     -I=Bin/protobuf/include `
     -I=proto proto/Service/* `
     -I=Bin/protobuf/include/protoc-gen-openapiv2/options `
-    --plugin=protoc-gen-openapiv2=$ProtobufPath/protoc-gen-openapiv2.exe `
+    --plugin=protoc-gen-openapiv2=$gRPCPath/protoc-gen-openapiv2.exe `
     --openapiv2_out=apiref/openapiv2 `
     --openapiv2_opt=generate_unbound_methods=true
 
